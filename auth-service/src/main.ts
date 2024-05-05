88a4843a3471648d917c583e95a3b7df64860d2d
nikita_grishin-sa <nikita.grishin>
2024-05-05 19:23:19 +0500

    add user and crud api

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+
+    const configService: ConfigService = app.get(ConfigService);
+    const port = configService.getOrThrow<number>('PORT_APP');
+
+    await app.listen(port);
+    console.log(`Started on port ${port}`);
 }
 bootstrap();
